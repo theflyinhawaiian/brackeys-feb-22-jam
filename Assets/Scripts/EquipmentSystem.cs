a47e85e4a6a927cd8ec69048edc8110d1c3d5166
Peter Mullins <jc310xc@gmail.com>
2022-02-21 20:40:08 -0500

    gee thanks, gitignore.io

--- Assets/Scripts/EquipmentSystem.cs
+++ Assets/Scripts/EquipmentSystem.cs
@@ -5,17 +5,20 @@ namespace Assets.Scripts
     class EquipmentSystem
     {
         private BaseWeapon gun;
+        private HitscanWeapon laser;
         private Transform origin;
 
         public EquipmentSystem(Transform origin)
         {
             this.origin = origin;
             gun = new BaseWeapon();
+            laser = new HitscanWeapon();
         }
 
         public void TryUseActiveItem()
         {
-            gun.FireBullet(origin);
+            //gun.FireBullet(origin);
+            laser.Fire(origin);
         }
     }
 }
