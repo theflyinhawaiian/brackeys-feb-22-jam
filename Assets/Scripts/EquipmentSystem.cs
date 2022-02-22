af4f54286cb535558557a6144f8e094a0af8f4f4
Elijah Davis <elijah@moonwatch.io>
2022-02-21 17:58:45 -0800

    pete is an idiot lol

--- Assets/Scripts/EquipmentSystem.cs
+++ Assets/Scripts/EquipmentSystem.cs
@@ -5,20 +5,17 @@ namespace Assets.Scripts
     class EquipmentSystem
     {
         private BaseWeapon gun;
-        private HitscanWeapon laser;
         private Transform origin;
 
         public EquipmentSystem(Transform origin)
         {
             this.origin = origin;
             gun = new BaseWeapon();
-            laser = new HitscanWeapon();
         }
 
         public void TryUseActiveItem()
         {
-            //gun.FireBullet(origin);
-            laser.Fire(origin);
+            gun.FireBullet(origin);
         }
     }
 }
